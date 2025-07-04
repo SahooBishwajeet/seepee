93f7aeb80cd3fd677741e4f71224daabf5c7b4d4
SahooBishwajeet <biswa.2004.bdk@gmail.com>
2025-07-04 23:26:51 +0530

    [SB] [Add] Template for C++ CP (SeePee)

---
+++ templates/template.cpp
@@ -0,0 +1,70 @@
+#include "bits/stdc++.h"
+
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define rep1(i, n) for (int i = 1; i < (n); ++i)
+#define rep1n(i, n) for (int i = 1; i <= (n); ++i)
+#define repr(i, n) for (int i = (n) - 1; i >= 0; --i)
+#define pb push_back
+#define eb emplace_back
+#define all(a) (a).begin(), (a).end()
+#define rall(a) (a).rbegin(), (a).rend()
+#define each(x, a) for (auto &x : a)
+#define ar array
+#define vec vector
+#define range(i, n) rep(i, n)
+
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using ld = double;
+using str = string;
+using pi = pair<int, int>;
+using pl = pair<ll, ll>;
+
+using vi = vector<int>;
+using vl = vector<ll>;
+using vpl = vector<pl>;
+using vpi = vector<pair<int, int>>;
+using vvi = vector<vi>;
+
+int Bit(int mask, int b) { return (mask >> b) & 1; }
+
+template <class T>
+bool ckmin(T &a, const T &b) {
+    if (b < a) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+template <class T>
+bool ckmax(T &a, const T &b) {
+    if (b > a) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+const int INF = 2e9;
+const int MOD = 1e9 + 7;
+const int MAX_N = 5e5 + 5;
+const ld EPS = 1e-9;
+
+void solve() {
+    // Code Here
+}
+
+signed main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cout << setprecision(15) << fixed;
+    int t = 1;
+    cin >> t;
+    rep(_, t) { solve(); }
+
+    return 0;
+}
